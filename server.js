0ede912a7ef0183ab88c5c0da21edd708609170f
Indra Gunawan <indragunawanrtf@gmail.com>
2018-04-20 20:29:03 +0700

    Forget Add console connecting

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
-mongoose.connect('mongodb://indra-gunawan:admin@ds251889.mlab.com:51889/gits-indonesia', {});
+mongoose.connect('mongodb://indra-gunawan:admin@ds251889.mlab.com:51889/gits-indonesia', () => {
+  console.log('Connecting To MongoDB OK');
+});
 
 app.use(bodyParser.json());
 
