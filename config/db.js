5311a7452bdb11fce68a3e2b4fa0449af4442b5f
Indra Gunawan <indragunawanrtf@gmail.com>
2018-04-22 04:53:23 +0700

    Updated MLAB_PW with ES6

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = app => {
   mongoose.connect(
-    "mongodb://indra-gunawan:"+ process.env.MLAB_PW +"@ds251889.mlab.com:51889/gits-indonesia",
+    `mongodb://indra-gunawan:${process.env.MLAB_PW}@ds251889.mlab.com:51889/gits-indonesia`,
     () => {
       console.log("Connecting To MongoDB OK");
     }
