60d3da546c93c4cb39218f05a607e98e9826fd56
Indra Gunawan <indragunawanrtf@gmail.com>
2018-04-21 21:40:47 +0700

    Refactor For Folder Controllers

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 // Routes
 const connectDB = require('./config/db')(app);
-const employee = require('./controllers/employees')(app);
-const portofolio = require('./controllers/portofolio')(app);
+const employee = require('./api/controllers/employees')(app);
+const portofolio = require('./api/controllers/portofolio')(app);
 const handlingError = require('./config/handling-error')(app);
 const port = require('./config/port')(app);
-
