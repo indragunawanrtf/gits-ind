d3daabd364e9a4b8ef9d34560ba5b8b61d833e90
Indra Gunawan <indragunawanrtf@gmail.com>
2018-04-21 20:18:32 +0700

    DELETE API portofolio

--- controllers/portofolio.js
+++ controllers/portofolio.js
@@ -15,22 +15,36 @@ module.exports = app => {
   // POST API Portofolio
   app.post("/api/portofolio", (req, res) => {
     const newPortofolio = new Portofolio(req.body);
-    newPortofolio.save().then(() => {
-      res.json({ message: "Portofolio Success Created"});
-    }).catch(err => {
-      console.error(err);
-    });
-  });
-  
-  // PUT API Portofolio
-  app.put('/api/portofolio/:id', (req, res) => {
-    Portofolio.update({ _id: req.params.id }, req.body)
-    .then(() => {
-      res.json({ message: "Portofolio Success Updated"})
-    })
-    .catch(err => {
-      console.error(err);
-    });
+    newPortofolio
+      .save()
+      .then(() => {
+        res.json({ message: "Portofolio Success Created" });
+      })
+      .catch(err => {
+        console.error(err);
+      });
   });
 
+  // PUT API Portofolio
+  app.put("/api/portofolio/:id", (req, res) => {
+    Portofolio.update({ _id: req.params.id }, req.body)
+      .then(() => {
+        res.json({ message: "Portofolio Success Updated" });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  });
+
+  // DELETE API Portofolio
+  app.delete("/api/portofolio/:id", (req, res) => {
+    Portofolio.remove({ _id: req.params.id })
+      .then(() => {
+        res.json({ message: "Portofolio Success Deleted" });
+      })
+      .catch(err => {
+        console.err(err);
+      });
+  });
+  
 };
