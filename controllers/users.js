9e0f0874c34600fa51961fbe56f56bfd981e228a
Indra Gunawan <indragunawanrtf@gmail.com>
2018-04-22 01:23:06 +0700

    POST API User

--- controllers/users.js
+++ controllers/users.js
@@ -1,1 +1,18 @@
-var User = require('./models/users');
+const User = require("../models/users");
+
+module.exports = app => {
+
+  // POST API User
+  app.post("/api/users", (req, res) => {
+    const newUser = new User(req.body);
+    newUser
+      .save()
+      .then(() => {
+        res.json({ message: "User Success Created" });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  });
+
+};
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 // Routes
 const connectDB = require('./config/db')(app);
+const user = require('./controllers/users')(app);
 const employee = require('./controllers/employees')(app);
 const portofolio = require('./controllers/portofolios')(app);
 const handlingError = require('./config/handling-error')(app);
